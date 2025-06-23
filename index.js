406fdf04bada67968968cbeabb5c7255f5ab9619
황세연 <syeonih@innogrid.com>
2025-06-23 15:22:50 +0900

    Mutation 구현

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 const database = require('./database')
 const { ApolloServer, gql } = require('apollo-server')
+
 const typeDefs = gql`
   type Query {
-    teams: [Team],
-    equipments: [Equipment],
-    supplies: [Supply],
-    team(id: Int): Team,
-
+    teams: [Team]
+    equipments: [Equipment]
+    supplies: [Supply]
+    team(id: Int): Team
   }
+
+  type Mutation {
+    insertEquipment(
+        id: String,
+        used_by: String,
+        count: Int,
+        new_or_used: String
+    ): Equipment,
+    editEquipment(
+        id: String,
+        used_by: String,
+        count: Int,
+        new_or_used: String
+    ): Equipment,
+    deleteEquipment(
+        id: String): Equipment
+  }
+
   type Team {
     id: Int
     manager: String
@@ -18,17 +36,20 @@ const typeDefs = gql`
     project: String
     supplies: [Supply]
   }
+
   type Equipment {
-    id: String,
-    used_by: String,
-    count: Int,
+    id: String
+    used_by: String
+    count: Int
     new_or_used: String
-}
+  }
+
   type Supply {
-    id: String,
+    id: String
     team: Int
-}
+  }
 `
+
 const resolvers = {
   Query: {
     teams: () => database.teams,
@@ -37,22 +58,47 @@ const resolvers = {
 
     // 특정 team만 받아오기
     team: (parent, args, context, info) => database.teams
-    .filter((team) => {
+      .filter((team) => {
         return team.id === args.id
-    })[0],
-    
+      })[0],
+
     // team에 supplies 연결해서 받아오기
     teams: () => database.teams
-    .map((team) => {
+      .map((team) => {
         team.supplies = database.supplies
-        .filter((supply) => {
+          .filter((supply) => {
             return supply.team === team.id
-        })
+          })
         return team
-    }),
+      }),
+  },
+
+  Mutation: {
+    insertEquipment: (parent, args, context, info) => {
+      database.equipments.push(args)
+      return args
+    },
+    editEquipment: (parent, args, context, info) => {
+      return database.equipments.filter((equipment) => {
+        return equipment.id === args.id
+      }).map((equipment) => {
+        Object.assign(equipment, args)
+        return equipment
+      })[0]
+    },
+    deleteEquipment: (parent, args) => {
+      const deleted = database.equipments.find((equipment) => {
+        return equipment.id === args.id
+      })
+      database.equipments = database.equipments.filter((equipment) => {
+        return equipment.id !== args.id
+      })
+      return deleted
+    }
   }
 }
+
 const server = new ApolloServer({ typeDefs, resolvers })
 server.listen().then(({ url }) => {
-console.log(`🚀  Server ready at ${url}`)
+  console.log(`🚀  Server ready at ${url}`)
 })
